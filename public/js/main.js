a9bc22f8389bd1adae63e10a3ce04b17fb720be7
Isaacudoh <udoh.nsikak@gmail.com>
2017-11-14 19:48:12 -0500

    Challenge completed

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,22 @@
  const buttons = document.querySelectorAll('button')
 
  buttons[0].addEventListener('click', colorChange);
+ buttons[1].addEventListener('click', changeText);
+ buttons[2].addEventListener('click', changeTextBack);
 
  function colorChange () {
    // buttons[0].classList.toggle('button-danger');
    //event.target.classlist.toggle('button-danger');
+   // use toggle for click events
    this.classList.toggle('button-danger');
  }
 
-})()
+ function changeText () {
+   this.innerText = 'you clicked me';
+ }
+
+ function changeTextBack (){
+   buttons[1].innerText = 'button two';
+ }
+ 
+})();
