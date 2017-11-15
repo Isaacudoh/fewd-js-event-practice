33ad710cddbb741e56e4f4ac8f3b630cbb0194be
Isaacudoh <udoh.nsikak@gmail.com>
2017-11-14 19:32:50 -0500

    more ways same result

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,9 @@
  buttons[0].addEventListener('click', colorChange);
 
  function colorChange () {
-   buttons[0].classList.toggle('button-danger');
+   // buttons[0].classList.toggle('button-danger');
+   //event.target.classlist.toggle('button-danger');
+   this.classList.toggle('button-danger');
  }
 
 })()
